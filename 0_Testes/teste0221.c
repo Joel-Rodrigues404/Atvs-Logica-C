850418611476dd4e7ab54cf64e187804383b04de
Joel-Rodrigues404 <joelsandersonrodrigues@gmail.com>
2024-12-25 20:12:17 -0300

    Atividades

---
+++ 0_Testes/teste0221.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("A = B == %d %d\n", A = B);
+    printf("A == %d\n", A);
+    return 0;
+}
--- 2_estrutura_decisao/atv05.c
+++ 2_estrutura_decisao/atv05.c
@@ -4,3 +4,19 @@ O programa deve calcular a média alcançada por aluno e apresentar: */
 /*     A mensagem "Aprovado", se a média alcançada for maior ou igual a sete; */
 /*     A mensagem "Reprovado", se a média for menor do que sete; */
 /*     A mensagem "Aprovado com Distinção", se a média for igual a dez.  */
+
+#include <stdio.h>
+
+int main() {
+    float nota1, nota2;
+    float media = (nota1 + nota2) / 2;
+
+    if (media == 10) {
+        printf("Aprovado com Distinção");
+    } else if (media >= 7) {
+        printf("Aprovado");
+    } else if (media < 7) {
+        printf("Reprovado");
+    }
+    return 0;
+}
